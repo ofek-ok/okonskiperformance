a886cfd65016202ebe08a18f18b53b9cf405df46
Ofek Okonski <kai@capitalmotorcars.com>
2026-08-30 13:42:15 +0300

    Zoom in on blue circle logo and add blue glow in Hero background

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,13 +6,16 @@ export default function Hero() {
       {/* Background Deep Gradient / Vignette */}
       <div className="absolute inset-0 z-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-steel/20 via-charcoal to-black" />
       
-      {/* Dimmed Logo Background */}
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] sm:w-[800px] sm:h-[800px] opacity-[0.05] z-0 pointer-events-none flex items-center justify-center">
+      {/* Huge subtle glow to emphasize the blue */}
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[60%] w-[600px] h-[600px] bg-brand/10 blur-[100px] rounded-full pointer-events-none z-0" />
+
+      {/* Dimmed Logo Background - Zoomed in on the Blue Circle */}
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[60%] w-[1000px] h-[1000px] sm:w-[1500px] sm:h-[1500px] opacity-[0.08] z-0 pointer-events-none flex items-center justify-center">
         <Image 
           src="/logo-official.png" 
           alt="Okonski Performance Background Logo" 
           fill
-          className="object-contain"
+          className="object-cover object-top scale-[1.5]"
           priority
         />
       </div>
